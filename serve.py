5c44969aa954b406ed43121cd79193efdbad64a5
Luke San Antonio Bialecki <lukesanantonio@gmail.com>
2016-08-28 10:35:38 -0400

    Don't use so any scopes

--- serve.py
+++ serve.py
@@ -63,10 +63,7 @@ def user_loader(id):
 SPOTIFY_CLIENT_ID = '642055f7320044edafd1d6290b5a4b57'
 SPOTIFY_CLIENT_SECRET = '77d07dc919034af598c6b228475a9ec3'
 SPOTIFY_REDIRECT_URL = 'http://www.redcrane.net/spotify_auth_callback'
-SPOTIFY_USE_SCOPE = ('playlist-read-private '
-                     'streaming '
-                     'user-follow-modify '
-                     'user-top-read ')
+SPOTIFY_USE_SCOPE = ('user-top-read ')
 
 @app.route('/')
 def index():
