c8510233e1e1230cf5710c23b7ec635700765017
Luke San Antonio Bialecki <lukesanantonio@gmail.com>
2016-10-25 20:18:42 -0400

    Remove credentials file

--- local_config.py
+++
@@ -1,7 +0,0 @@
-SECRET_KEY = 'HDKaskjayuwq5163h1bdsbhfihds'
-
-# == Spotify Credentials
-SPOTIFY_CLIENT_ID = '642055f7320044edafd1d6290b5a4b57'
-SPOTIFY_CLIENT_SECRET = '77d07dc919034af598c6b228475a9ec3'
-SPOTIFY_REDIRECT_URL = 'http://localhost:5000/spotify_auth_callback'
-SPOTIFY_USE_SCOPE = ('user-top-read ')
